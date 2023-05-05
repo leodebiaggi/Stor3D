23c1bd882b7ebaa312cc583dc71bd56fa701e5e7
Leonel Debiaggi <leodebiaggi@gmail.com>
2023-05-05 10:50:57 -0300

    Eliminacion Slider - Creacion ItemCount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import SliderContainer from './components/SliderContainer/SliderContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting={'Bienvenidos a Stor3D'} />
-      <SliderContainer />
+      <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ',quantity)}/>
     </div>
   );
 }
